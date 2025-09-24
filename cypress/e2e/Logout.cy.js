b22032e8d18238fdc9173125d45ff8796b1e5938
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2025-09-24 20:33:22 -0300

    Update Logout.cy.js

--- cypress/e2e/Logout.cy.js
+++ cypress/e2e/Logout.cy.js
@@ -5,6 +5,5 @@ describe('template spec', () => {
   it('Home Page', () => {
     cy.LoginPage()
     cy.logoutPage()
-    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/auth/login`)
   })
 })
